650fb2044becf9db8548e20ea44a5d26d33042e4
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-03 01:11:04 +0800

    add a helper marking each review's rating

---
+++ app/helpers/comment-rating.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export function commentRating(params/*, hash*/) {
+  var review = params[0];
+
+  if (review.get('rating') === 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span>');
+  } else if (review.get('rating') === 4) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if (review.get('rating') === 3) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if (review.get('rating') === 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if (review.get('rating') === 1) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  }
+}
+
+export default Ember.Helper.helper(commentRating);
---
+++ tests/unit/helpers/comment-rating-test.js
@@ -0,0 +1,12 @@
+
+import { commentRating } from 'books-reviews/helpers/comment-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | comment rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = commentRating([42]);
+  assert.ok(result);
+});
+
