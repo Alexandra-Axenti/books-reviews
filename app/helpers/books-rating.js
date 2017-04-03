3c45bc9855986be7728ad9e7270479ba3547ecfd
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-03 17:51:52 +0800

    add the half-filled glyphicon

---
+++ app/helpers/books-rating.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export function booksRating(params/*, hash*/) {
+  var bookRating = params[0]
+
+  if (bookRating === 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon"><image src="./public/resources/images/half.png"></image></span><span class="glyphicon"><image src="./public/resources/images/half.png"></image></span>');
+  } else if ((bookRating >= 4.5) && (bookRating < 5)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span><image class="glyph" src="/assets/half.png"></image></span>');
+  } else if ((bookRating >= 4) && (bookRating < 4.5)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 3.5) && (bookRating < 4)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"><span><image class="glyph" src="/assets/half.png"></image></span></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 3) && (bookRating < 3.5)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 2.5) && (bookRating < 3)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span><image class="glyph" src="/assets/half.png"></image></span></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 2) && (bookRating < 2.5)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 1.5) && (bookRating < 2)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span><image class="glyph" src="/assets/half.png"></image></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  } else if ((bookRating >= 1)) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span>');
+  }
+}
+
+export default Ember.Helper.helper(booksRating);
---
+++ tests/unit/helpers/books-rating-test.js
@@ -0,0 +1,12 @@
+
+import { booksRating } from 'books-reviews/helpers/books-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | books rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = booksRating([42]);
+  assert.ok(result);
+});
+
